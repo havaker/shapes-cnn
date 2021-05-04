cc2daac2011537baa1faf036cea13b282ed92201
Michał Sala <0havaker@gmail.com>
2021-05-04 20:37:09 +0200

    better extract script

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -2,6 +2,12 @@
 
 cd "$(dirname "$0")"
 
+mkdir -p ../data
 cd ../data
+
+if [ ! -f gsn-2021-1.zip ]; then
+    wget https://www.mimuw.edu.pl/~ciebie/gsn-2021-1.zip
+fi
+
 unzip ../data/gsn-2021-1.zip
 mv data extracted
